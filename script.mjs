1c2c0ee8433896090c3dfd09077296e2e0a505f3
Alka Sah <alkasah485@gmail.com>
2024-05-12 18:21:29 -0700

    Part 1 return average

--- script.mjs
+++ script.mjs
@@ -1,5 +1,6 @@
 // Part 1: Thinking Functionality
 
+//Take an array of numbers and return the sum.
 function sumOfArray (arr) {
     let sum = 0;
     for (let i = 0 ; i < arr.length ; i++){
@@ -10,4 +11,20 @@ function sumOfArray (arr) {
 
 }
 
-sumOfArray([1,2,3,4])
+sumOfArray([1,2,3,4]);
+
+//Take an array of numbers and return the average.
+
+function avgOfArray (arr) {
+    let sum = 0;
+    let num = arr.length;
+    for (let i = 0 ; i <arr.length; i++){
+        sum += arr[i]
+    }
+
+    let avg = sum / num;
+
+    console.log(avg);
+}
+
+avgOfArray([1,2,3,4]);
